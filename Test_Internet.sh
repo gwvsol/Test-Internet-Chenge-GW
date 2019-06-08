9c260df45a30ac0191e30a2e4f43da45f7eb25c0
gwvsol <gwvsol@gmail.com>
2019-06-08 23:24:04 +0300

    добавлены вспомогательные функции скрипта

--- Test_Internet.sh
+++ Test_Internet.sh
@@ -4,8 +4,6 @@ host=google.com
 gw1="DD-WRT" # имена и IP адреса необходимо прописать в /etc/hosts
 gw2="ASUS"
 log="/var/log/Test_Internet.log"
-# Если используется монитор conky раскоментировать строку
-#Conky="/mining/.log/.testInternetConky.msg"
 
 # =============== Сообщения для LOG файла ===================
 Gateway1="Интернет $gw1"
@@ -19,7 +17,7 @@ NoGW2="Интернет на $gw2 не доступен"
 # ============================================================
 
 # =========== Основная функция скрипта =======================
-# Проверяем на каком шлюзе включен интернет - основная функция скрипта
+# Проверяем на каком шлюзе включен интернет
 ifgw()
 {
 dgw=($(route | grep default | awk '{print $2}'))
@@ -29,7 +27,6 @@ dgw=($(route | grep default | awk '{print $2}'))
 if [ $dgw == $gw1 ] ; then
     echo $(date +'%d-%m-%Y %T') "$Gateway1" >> $log
     echo "$Gateway1"
-    #echo "$Gateway1" > $Conky
     testInternet
     chengGW2
 fi
@@ -42,9 +39,9 @@ if [ $dgw == $gw2 ] ; then
     echo $(date +'%d-%m-%Y %T') "$SwitchGW1" >> $log
     #echo "$SwitchGW1"
     # Удаляем GW2 
-    sudo route del default gw $gw2
+    route del default gw $gw2
     # Добавляем GW1
-    sudo route add default gw $gw1
+    route add default gw $gw1
     echo $(date +'%d-%m-%Y %T') "$CHeckInetGW1" >> $log
     #echo "$CHeckInetGW1"
     testInternet
@@ -52,3 +49,51 @@ if [ $dgw == $gw2 ] ; then
 fi
 }
 # ============================================================
+
+# ============= Проверка доступности интернета ===============
+# Проверяем 10 раз и ограничиваем время проверки 30 секндами
+testInternet()
+{
+    ping $host -c 10 -w 30 &>/dev/null
+    rezTest=$?
+}
+# ============================================================
+
+# === Переключение на GW2 и проверка доступности интернета ===
+# Используется если интернет по GW1 не доступен
+# Переключем на GW2
+chengGW2()
+{
+if [[ $rezTest -eq 0 ]]; then
+    # Если интернет на GW1 доступен, выходим из скрипта
+    echo $(date +'%d-%m-%Y %T') "$OnLine" >> $log
+    #echo "$OnLine"
+    exit 0
+else
+    # Если не доступен...
+    echo $(date +'%d-%m-%Y %T') "$NoGw1SwGW2" >> $log
+    #echo "$NoGw1SwGW2"
+    # Удаляем GW1
+    route del default gw $gw1
+    # Добавляем GW2 и проверяем наличие интернета на GW2
+    route add default gw $gw2
+    echo $(date +'%d-%m-%Y %T') "$CHeckInetGW2" >> $log
+    #echo "$CHeckInetGW2"
+    testInternet
+if [[ $rezTest -eq 0 ]]; then
+    echo $(date +'%d-%m-%Y %T') "$OnLine" >> $log
+    #echo "$OnLine"
+    exit 0
+else
+    echo $(date +'%d-%m-%Y %T') "$NoGW2" >> $log
+    #echo "$NoGW2"
+    exit 1
+fi
+fi
+}
+# ===========================================================
+
+# ============== Запуск работы скрипта ======================
+ifgw
+# ===========================================================
+
